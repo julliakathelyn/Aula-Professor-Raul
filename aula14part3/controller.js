4afb8feeba934015c3f9667bfab6de59aceeaa08
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-05-28 11:21:35 -0300

    Angular

---
+++ aula14part3/controller.js
@@ -0,0 +1,8 @@
+var app = angular.module("paisesApp",[]);
+
+app.controller('PaisesController', function($scope){
+    $scope.capitais = [
+        {capital: "Brasilia", pais: "Brasil"},
+        {capital: "Madrid", pais: "Espanha"},
+        {capital: "Berlim", pais: "Alemanha"}];
+    });
---
+++ aula14part4/controller_carrinho.js
@@ -0,0 +1,13 @@
+//criação do app
+var app = angular.module("carrinhoApp",[]);
+app.controller("CarrinhoController", function ($scope){
+    //Criando um Array com os elementos
+    $scope.items = [
+        { nome: "Cabo HDMI", quantidade: 1, preco: 22.00},
+        { nome: "Teclado", quantidade: 3, preco: 300.00},
+        { nome: "Pen Drive", quantidade:2, preco: 100.00}
+    ];
+    $scope.remove = function(index){
+        $scope.items.splice(index,1);
+    }
+});
